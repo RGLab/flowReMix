51b31f227b8bb144a38673b9f4afe951d1b6c409
amitmeir <amitmeir@uw.edu>
2016-12-21 11:28:57 -0600

    RV144 analysis without outliers

--- testing GLMER function.R
+++ testing GLMER function.R
@@ -1,11 +1,17 @@
 library(flowReMix)
+library(pROC)
 cummean <- function(x) cumsum(x) / 1:length(x)
 data(rv144)
 #set.seed(502)
+omit <- paste("P", c(1001, 1013, 1019, 1023, 1031, 1034, 1039, 1045,
+                     1060, 1095, 1099, 1100, 1109, 1177, 1180, 1187,
+                     1201, 1215, 1216, 1224, 1227, 1232, 1242, 1284),
+              sep = "")
 par(mfrow = c(1, 1), mar = rep(4, 4))
 data <- rv144
+data <- subset(data, !(ptid %in% omit))
 leaves <- unique(data$population)
-selected_populations = c(1:3, 5:7)
+selected_populations = c(1:4, 5:7)
 data <- subset(data, population %in% leaves[selected_populations])
 data$population <- factor(data$population)
 data <- subset(data, stim != "sebctrl")
