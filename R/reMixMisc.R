2ed451dc4e3efc90d0696c1a6a4482c047a1e714
gfinak <greg.finak@gmail.com>
2017-10-04 10:46:05 -0700

    add a default seed to flowReMix, rather than NULL

--- R/reMixMisc.R
+++ R/reMixMisc.R
@@ -49,7 +49,7 @@ flowReMix_control <- function(updateLag = 5, randomAssignProb = 0.0, nsamp = 20,
                               maxDispersion = 10^3, minDispersion = 10^8, isingInit = -4,
                               keepEach = 5, centerCovariance = TRUE, intSampSize = 100,
                               initMethod = NULL, ncores = NULL, preAssignCoefs = 0,
-                              markovChainEM = TRUE, seed=NULL, prior = 0,
+                              markovChainEM = TRUE, seed=100, prior = 0,
                               isingWprior = FALSE) {
 
   object <- list(updateLag = updateLag,
